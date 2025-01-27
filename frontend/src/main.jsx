e2ec6a0b507b7da58784cdb17c120df162cde373
satyam <satyamb971@gmail.com>
2025-01-27 12:16:10 +0530

    correc ted code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,6 @@ import { persistStore } from 'redux-persist'
 let persistor=persistStore(store)
 import axios from 'axios'
 import { server } from './constant/config.js'
-// axios.defaults.baseURL = 'https://instagram-clone-3-kthd.onrender.com'
 axios.defaults.baseURL = server
 
 createRoot(document.getElementById('root')).render(
